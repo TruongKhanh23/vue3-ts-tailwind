b02a0539e944e8e72593278e7464c09e96795708
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-12-21 17:34:52 +0700

    feat: Navigator

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <Navigator />
     <div class="max-w-3xl mx-auto my-6">
       <router-view />
     </div>
@@ -9,7 +10,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import Navigator from "./components/Navigator.vue"
+
 export default defineComponent({
-  components: {},
+  components: {
+    Navigator,
+  },
 })
 </script>
